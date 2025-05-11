21f63e91876517bbc88c4aeba92b3ee211852113
aaryyann <arya000045@gmail.com>
2025-05-12 03:48:26 +0530

    csv removed json updated

--- utils/flagged-keywords.ts
+++ utils/flagged-keywords.ts
@@ -1,20 +1,5 @@
-import fs from "fs";
-import csv from "csv-parser";
+import symptoms from "../public/symptoms.json";
 
-function loadSymptomsFromCSV(filePath: string): Promise<string[]> {
-  return new Promise((resolve, reject) => {
-    const results: string[] = [];
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on("data", (row) => results.push(row["Symptoms"]))
-      .on("end", () => resolve(results))
-      .on("error", reject);
-  });
-}
-
-// This function returns the list of symptoms
 export async function riskKeywords(): Promise<string[]> {
-  const symptoms = await loadSymptomsFromCSV("public/symptoms.csv");
   return symptoms;
 }
-
